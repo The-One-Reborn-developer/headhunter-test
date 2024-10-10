fe6ab6244a30bfcd24e07e867470eb21cca87d9a
The One Reborn <mortonmarrion@gmail.com>
2024-10-10 13:40:51 +0300

    feat: add rss router set up

--- app/routers/add_rss.py
+++ app/routers/add_rss.py
@@ -1,6 +1,22 @@
 from aiogram import Router, F
+from aiogram.types import CallbackQuery
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+from app.keyboards.back_to_main_keyboard import back_to_main_keyboard
+
+
+class AddRss(StatesGroup):
+    rss_url = State()
 
 
 add_rss_router = Router()
 
 
+@add_rss_router.callback_query(F.data == 'add_rss')
+async def add_rss(message: CallbackQuery, state: FSMContext):
+    await state.set_state(AddRss.rss_url)
+
+    content = 'Пожалуйста, предоставьте валидный URL RSS источника 😸'
+
+    await message.message.edit_text(content, reply_markup=back_to_main_keyboard())
